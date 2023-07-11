d103048139d518c5dace0d17a16bb625d9446913
Wan-Hsuan Lee <lee00834@algonquinlive.com>
2023-07-10 21:22:21 -0400

    Create ImageGenerator Activity

--- app/src/main/java/algonquin/cst2335/finalproject/ImageGenerator.java
+++ app/src/main/java/algonquin/cst2335/finalproject/ImageGenerator.java
@@ -3,11 +3,11 @@ package algonquin.cst2335.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ImageGenerator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_image_generator);
     }
 }
