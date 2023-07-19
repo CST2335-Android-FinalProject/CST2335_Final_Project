42cee49b83af470e8c56aba06f54161f3892c44c
Wan-Hsuan Lee <lee00834@algonquinlive.com>
2023-07-19 19:55:46 -0400

    Create 4 setOnClickListener()

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,13 +1,39 @@
 package algonquin.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        binding.flight.setOnClickListener( clckk-> {
+//            Intent flightTracker = new Intent( MainActivity.this, FlightTracker.class);
+//            startActivity( flightTracker );
+//        });
+//
+//        binding.exchange.setOnClickListener( clckk-> {
+//            Intent currencyConverter = new Intent( MainActivity.this, CurrencyConverter.class);
+//            startActivity( currencyConverter );
+//        });
+//
+//        binding.question.setOnClickListener( clckk-> {
+//            Intent triviaQuestion = new Intent( MainActivity.this, TriviaQuestion.class);
+//            startActivity( triviaQuestion );
+//        });
+//
+//        binding.bear.setOnClickListener( clckk-> {
+//            Intent imageGenerator = new Intent( MainActivity.this, ImageGenerator.class);
+//            startActivity( imageGenerator );
+//        });
     }
 }
